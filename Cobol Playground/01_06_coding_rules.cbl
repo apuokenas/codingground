b4270b03589833b696e65410b620e6547c2b8ae6
Mantas Tumėnas <mantas@tumenas.eu>
2017-03-31 03:54:18 +0300

    Created by Coding Ground Client

---
+++ Cobol Playground/01_06_coding_rules.cbl
@@ -0,0 +1,32 @@
+000001 IDENTIFICATION DIVISION.
+000002 PROGRAM-ID.   SAMPLE as "SAMPLE".
+000003 AUTHOR. 	   MANTAS TUMENAS.
+       DATE-WRITTEN. 2017-03-31.
+      ******************************************************************************
+      *            This program asks the user for their name and says hello        *
+      ******************************************************************************
+000004 ENVIRONMENT DIVISION.
+000005 CONFIGURATION SECTION.
+000006 SOURCE-COMPUTER. 
+000007 OBJECT-COMPUTER.
+
+000008 INPUT-OUTPUT SECTION.
+
+
+000009 DATA DIVISION.
+000010 FILE SECTION.
+
+000011 WORKING-STORAGE SECTION.
+000012 01  WS-NAME	PIC X(10).
+
+
+000013 PROCEDURE DIVISION.
+000014 0001-HELLO-WORLD.
+
+000015	   DISPLAY "Enter your first name, please:".
+000016	   ACCEPT WS-NAME. 
+000017	   DISPLAY "Hello, ", WS-NAME.
+
+000018     STOP RUN.
+
+000019     END PROGRAM SAMPLE.
